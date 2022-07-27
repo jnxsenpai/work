a8744d70badb18645466459e5c3a440c0d98349a
Maksim <jnxsenpai@yandex.ru>
2022-07-27 19:52:00 +0300

    1

--- training_projects/js_practic/js/script.js
+++ training_projects/js_practic/js/script.js
@@ -1,23 +1,10 @@
 "use strict";
 
-let number = 4.6;
-console.log(-4/0);
-console.log('string' * 9);
+let incr = 10,
+    decr = 10;
 
-const person = 'Alex';
+incr++;
+decr--;
 
-const bool = true;
-
-let und;
-console.log(und);
-
-const obj = {
-    name: 'John',
-    age: 25,
-    isMarried: false
-};
-
-console.log(obj.name);
-
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-console.log(arr[1]);
+console.log(incr);
+console.log(decr);
